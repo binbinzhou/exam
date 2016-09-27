65fcc668da5ba3d623ab6569cbbb913ddea4fab9
binbinzhou <zhou0323>
2016-09-27 21:34:47 +0800

    20160927

--- js/index.js
+++ js/index.js
@@ -18,28 +18,42 @@ $(function () {
     $(".baseUI>li>ul>li").off("click");
     $(".baseUI>li>ul>li").on("click",function () {
         if(!$(this).hasClass("current")){
-            $(this).addClass("current").siblings().removeClass("current");
+            $(".baseUI>li>ul>li").removeClass("current");
+            $(this).addClass("current");
         }
     });
-
-
-
 });
 
-
 //核心模块
-angular.module("app",["ng","ngRoute","app.subject"])      //ng???
+angular.module("app",["ng","ngRoute","app.subject"])
     //核心模块
     .controller("mainCtrl",["$scope",function ($scope) {
         
     }])
-    //路由配置
-    /*
-    * a     题型id
-    * b     方向id
-    * c     知识点id
-    * d     难度id
+    //
+    /*设置a、b、c、d
+       a     题型id
+     * b     方向id
+     * c     知识点id
+     * d     难度id
+     * 使用的是后台过滤：前台只需获取到过滤的参数id传给后台，后台再把数据发送过来。
+     * 思路：
+     * 1、路由配置(index.html中的a标签需跟路由的路径一致，当选项为全部的时候变量都为0，
+    而subjectList中a标签也需要跟路由匹配)
+    需要在控制器中注入$routeParams，获取路径中的参数，再将$routeParams绑定到params中：$scope.params = $routeParams;
     *
+    *2、利用占位符，当做变量(在subjectList中a标签，当选项为全部的时候类型id为0,也就是a为0，而其他变量为params.b或其他，
+     而在遍历的a标签中，已知后面已经遍历出来type，所以将a后面的变量设置为type.id，所以当你点击哪个哪个的id就为type.id，
+     其他则为params.a或其他，)
+
+    * 3、添加active3样式(利用active3与他所在的行中做一个三目运算，例：在题目类型中，当选项为全部时：{{params.a == 0?'active3':'' }}
+    * 只有当id为0是才会给他添加active3样式，否则为空，这样当我们点击当选多选简答的时候active都为空。在遍历的a标签中：{{params.a == type.id?'active3':'' }}
+    * 当有id的时候就给这个添加一个active3，若没有id就为空。)
+    *
+    * 4、在获取所有题目信息的服务中，获取的时候给后台传一个参数，params，若参数有东西就会鼓过滤，若没有则把全部信息显示，
+    * 由于我们在前台获取的四个id是由abcd来保存，后台无法识别，所以要将四个保存id的变量用后台指定的变量来保存，前后台会规定格式，
+    *用一个data空对象来接收装换好的值，首先遍历params，判断条件当值不为0的时候才用switch语句转换值，最后把data传给get获取数据中的
+    * params让后台过滤
     * */
     .config(["$routeProvider",function ($routeProvider) {
         $routeProvider.when("/AllSubject/a/:a/b/:b/c/:c/d/:d",{
@@ -48,8 +62,14 @@ angular.module("app",["ng","ngRoute","app.subject"])      //ng???
         }).when("/SubjectManager",{
             templateUrl:"tpl/subject/subjectManager.html",
             controller:"subjectController"
-        }).when("/subjectAdd",{
+        }).when("/SubjectAdd",{
             templateUrl:"tpl/subject/subjectAdd.html",
             controller:"subjectController"
+        }).when("/SubjectDel/id/:id",{
+            templateUrl:"tpl/subject/subjectList.html",
+            controller:"subjectDelController"
+        }).when("/SubjectAud/id/:id/checkState/:checkState",{
+            templateUrl:"tpl/subject/subjectList.html",
+            controller:"subjectAudController"
         });
     }]);
--- js/subject.js
+++ js/subject.js
@@ -3,90 +3,193 @@
  * 题目管理的js模块
  */
 angular.module("app.subject",["ng","ngRoute"])
-    .controller("subjectController",["$scope","$location","SubjectService","SubjectsAllService","$routeParams",
-        function ($scope,$location,SubjectService,SubjectsAllService,$routeParams) {
+    .controller("subjectDelController",["SubjectsAllService","$routeParams","$location",
+        function (SubjectsAllService,$routeParams,$location) {
+            var flag = confirm("确认删除吗？");
+            if(flag){
+                var id = $routeParams.id;
+                SubjectsAllService.delSubject(id,function (data) {
+                    alert(data);
+                    //删除成功后页面跳转回题目列表页
+                    $location.path("/AllSubject/a/0/b/0/c/0/d/0");
+                });
+            } else {
+                $location.path("/AllSubject/a/0/b/0/c/0/d/0");
+            }
+
+    }])
+    .controller("subjectAudController",["SubjectsAllService","$routeParams","$location",
+        function (SubjectsAllService,$routeParams,$location) {
+                var id = $routeParams.id;
+                var checkState = $routeParams.checkState;
+            console.log(checkState);
+                SubjectsAllService.audSubject(id,checkState,function (data) {
+                    alert(data);
+                    $location.path("/AllSubject/a/0/b/0/c/0/d/0");
+                });
+    }])
+    .controller("subjectController",["$scope","SubjectService","SubjectsAllService","$routeParams","$location",
+        function ($scope,SubjectService,SubjectsAllService,$routeParams,$location) {
         //将路由参数绑定到作用域当中
             $scope.params = $routeParams;
-
+            //添加题目页面下拉框信息数据
             $scope.subject = {
-                typeId :1,
-                departmentId:1,
+                typeId :3,
                 levelId:1,
+                departmentId:1,
                 topicId:1,
                 stem:"",
                 answer:"",
-                analysis:""
+                analysis:"",
+                choiceContent:[],
+                choiceCorrect:[false,false,false,false]
             };
-            //调用添加题目信息方法
+            //保存并继续
             $scope.submit = function () {
-                SubjectService.saveSubject($scope,subject,function (data) {
-                   console.log(data);
+                SubjectsAllService.saveSubject($scope.subject,function (data) {
+                   alert(data);     //数据库中的提示消息
                 });
+                var subject = {
+                    typeId :1,
+                    levelId:1,
+                    departmentId:1,
+                    topicId:1,
+                    stem:"",
+                    answer:"",
+                    analysis:"",
+                    choiceContent:[],
+                    choiceCorrect:[false,false,false,false]
+                };
+                //重置作用域中绑定的表单默认值
+                angular.copy(subject,$scope.subject);
             };
 
-        //创建单个添加方法，绑定到html中
-        $scope.add = function () {
-            //设置路径，配合路由
-            $location.path("/subjectAdd");
-        };
-        //调用获取类型函数
-        SubjectService.getSubjecttypes(function (data) {
-            $scope.types = data;
-        });
-        //调用获取方向函数
-        SubjectService.getSubjectdepartments(function (data) {
-            $scope.departments = data;
-        });
-        //调用获取知识点函数
-        SubjectService.getSubjecttopics(function (data) {
-            $scope.topics = data;
-        });
-        //调用获取难度函数
-        SubjectService.getSubjectlevel(function (data) {
-            $scope.levels = data;
-        });
-        //调用所有题目信息
-        SubjectsAllService.getAllSubject($routeParams,function (data) {
-            //对每个题目进行遍历
-            data.forEach(function (subject) {
-                //为每个选项添加ABCD编号
-                var answer = [];
-                //对每个选择题里面的选项进行遍历
-                if(subject.subjectType.id != 3){
-                    subject.choices.forEach(function (choice,index) {
-                        //给选项组数里面的每一个选项设置一个属性no，调用一个方法将索引值转换为对应的ABCD
-                        choice.no = SubjectService.convertIndexToNo(index);
-                    });
-                    //如果题目类型id不为3，也就是为单选或者多选的时候
-                    if(subject.subjectType.id != 3){
-                        subject.choices.forEach(function (choice) {
-                            //如果选项的值为true则将序号添加进数组中
-                            if(choice.correct){
-                                answer.push(choice.no);
-                            }
-                        });
-                        //将数组转化为字符串格式
-                        subject.answer = answer.toString();
-                    }
-                }
+            //保存并关闭
+            $scope.submitAndClose = function () {
+                SubjectsAllService.saveSubject($scope.subject,function (data) {
+                    alert(data);
+                });
+                //保存并关闭，
+                $location.path("/AllSubject/a/0/b/0/c/0/d/0");
+            };
+
+            //调用获取类型函数
+            SubjectService.getSubjecttypes(function (data) {
+                $scope.types = data;
             });
-            $scope.subjects = data;
-        });
+            //调用获取方向函数
+            SubjectService.getSubjectdepartments(function (data) {
+                $scope.departments = data;
+            });
+            //调用获取知识点函数
+            SubjectService.getSubjecttopics(function (data) {
+                $scope.topics = data;
+            });
+            //调用获取难度函数
+            SubjectService.getSubjectlevel(function (data) {
+                $scope.levels = data;
+            });
+            //调用所有题目信息
+            SubjectsAllService.getAllSubject($routeParams,function (data) {
+                //对每个题目进行遍历
+                data.forEach(function (subject) {
+                    //为每个选项添加ABCD编号
+                    if(subject.subjectType != null){
+                        //对每个选择题里面的选项进行遍历
+                        if(subject.subjectType.id != 3){
+                            var answer = [];
+                            subject.choices.forEach(function (choice,index) {
+                                //给选项组数里面的每一个选项设置一个属性no，调用一个方法将索引值转换为对应的ABCD
+                                choice.no = SubjectService.convertIndexToNo(index);
+                            });
+                            //如果题目类型id不为3，也就是为单选或者多选的时候
+                            if(subject.subjectType.id != 3){
+                                subject.choices.forEach(function (choice) {
+                                    //如果选项的值为true则将序号添加进数组中
+                                    if(choice.correct){
+                                        answer.push(choice.no);
+                                    }
+                                });
+                                //将数组转化为字符串格式
+                                subject.answer = answer.toString();
+                            }
+                        }
+                    }
+
+                });
+                $scope.subjects = data;
+            });
     }])
     //获取全部题目的信息，创建一个新的服务，方便之后的增删该查，
-    .service("SubjectsAllService",["$http",function ($http) {
+    .service("SubjectsAllService",["$http","$httpParamSerializer",function ($http,$httpParamSerializer) {
+        //删除题目
+        this.delSubject = function( id,handler){
+          $http.get("http://172.16.0.5:7777/test/exam/manager/delSubject.action",{
+              params:{
+                  "subject.id":id
+              }
+          }).success(function (data) {
+              handler(data);
+          })
+        };
+        //审核题目
+        this.audSubject = function (id,checkState,handler) {
+            $http.get("http://172.16.0.5:7777/test/exam/manager/checkSubject.action",{
+                params:{
+                    "subject.id":id,
+                    "subject.checkState":"通过"
+                }
+            }).success(function (data) {
+                handler(data);
+            })
+        };
         //保存添加题目信息
         this.saveSubject = function (params,handler) {
             //处理数据
             var obj = {};
-
+            for(var key in params){
+                var val = params[key];
+                switch (key){
+                    case "typeId":
+                        obj['subject.subjectType.id'] = val;
+                        break;
+                    case "departmentId":
+                        obj['subject.department.id'] = val;
+                        break;
+                    case "levelId":
+                        obj['subject.subjectLevel.id'] = val;
+                        break;
+                    case "topicId":
+                        obj['subject.topic.id'] = val;
+                        break;
+                    case "stem":
+                        obj['subject.stem'] = val;
+                        break;
+                    case "answer":
+                        obj['subject.answer'] = val;
+                        break;
+                    case "analysis":
+                        obj['subject.analysis'] = val;
+                        break;
+                    case "choiceContent":
+                        obj['choiceContent'] = val;
+                        break;
+                    case "choiceCorrect":
+                        obj['choiceCorrect'] = val;
+                        break;
+                }
+            }
             console.log(obj);
-            $http.post("http://172.16.0.5:7777/test/exam/manager/saveSubject.action",obj)
-                .success(function (data) {
+            //对obj对象进行表单格式的序列化，(默认使用json格式)
+            obj = $httpParamSerializer(obj);
+            $http.post("http://172.16.0.5:7777/test/exam/manager/saveSubject.action",obj,{
+                headers:{
+                    "Content-Type":"application/x-www-form-urlencoded"
+                }
+            }).success(function (data) {
                     handler(data);
                 });
         };
-
 
         //获取所有题目信息
         //function中要传一个params参数
@@ -113,7 +216,7 @@ angular.module("app.subject",["ng","ngRoute"])
                     }
                 }
             }
-            console.log(data);
+            //console.log(data);
             //$http.get("data/subjects.json").success(function (data) {
                $http.get("http://172.16.0.5:7777/test/exam/manager/getAllSubjects.action",{
                   params:data
@@ -157,4 +260,55 @@ angular.module("app.subject",["ng","ngRoute"])
                 });
             }
         };
-    }]);
+    }])
+    .filter("selectTopic",function () {
+        //input要过滤的内容，方向id
+        return function (input,id) {
+            //数组中filter过滤方法
+            if(input){
+                var result = input.filter(function (item) {
+                    return item.department.id == id;
+                });
+                //将过滤后的返回
+                return result;
+            }
+        }
+    })
+    .directive("selectOption",function () {
+        return {
+            restrict:"A",
+            link:function (scope,element) {
+                element.on("change",function () {
+                    var type = $(this).attr("type");
+                    var val = $(this).val();
+                    var isChecked = $(this).prop("checked");
+                    //console.log(val);
+                    if(type == "radio"){
+                        //重置选项
+                        scope.subject.choiceCorrect = [false,false,false,false];
+                        for( var i = 0;i<4;i++ ){
+                            if(i==val){
+                                scope.subject.choiceCorrect[i] = true;
+                            }
+                        }
+                    } else if(type == "checkbox"){
+                        for( var i = 0;i<4;i++ ){
+                            if(isChecked){
+                                if(i==val){
+                                    scope.subject.choiceCorrect[i] = true;
+                                }
+                            }else{
+                                if(i==val){
+                                    scope.subject.choiceCorrect[i] = false;
+                                }
+                            }
+
+                        }
+                    }
+                    //强制消化
+                    scope.$digest();
+                })
+            }
+        }
+
+    });
